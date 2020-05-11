3cb5c23d9e4cde84837eca0404d8a6571f3e66da
TcaciucIulian <iulian.marius83@yahoo.ro>
2020-05-11 14:55:29 +0300

    Adaugare exercitii

---
+++ preProduction/exercitii/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:mds_project/all_ex.dart';
+
+import './all_ex.dart';
+import 'muscle_menu.dart';
+import 'side_menu.dart';
+
+void main()  => runApp(MainPage());
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ALL EXERCISES', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blueGrey,
+        ),
+        drawer: SideMenu(),
+        body: new ListView(
+          children: <Widget>[
+            MuscleMenu(),
+            Container(
+                height: 500,
+                child: Exercises()
+            )
+          ],
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+    );
+  }
+}
---
+++ preProduction/exercitii/lib/muscle_menu.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'muscle_category_ex.dart';
+
+class MuscleMenu extends StatefulWidget {
+  @override
+  _MuscleMenuState createState() => _MuscleMenuState();
+}
+
+class _MuscleMenuState extends State<MuscleMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 80,
+        child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Center(
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    color: Colors.white,
+                    child:  ListTile(
+                        title: Text('Chest', style: TextStyle(fontSize: 15, color: Colors.blueGrey), textAlign: TextAlign.center),
+                        onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                    builder: (context)  => GroupEx(
+                                    groupTitle: "Chest"
+                                    )
+                              )
+                            );
+                        },
+                    ),
+                  )
+              ),
+              Center(
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    color: Colors.white,
+                    width: 100,
+                    child:  ListTile(
+                      title: Text('Back', style: TextStyle(fontSize: 15, color: Colors.blueGrey), textAlign: TextAlign.center,),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context)  => GroupEx(
+                                    groupTitle: "Back"
+                                )
+                            )
+                        );
+                      },
+                    ),
+                  )
+              ),
+              Center(
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    width:100,
+                    color: Colors.white,
+                    child:  ListTile(
+                      title: Text('Biceps', style: TextStyle(fontSize: 15, color: Colors.blueGrey), textAlign: TextAlign.center,),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context)  => GroupEx(
+                                    groupTitle: "Biceps"
+                                )
+                            )
+                        );
+                      },
+                    ),
+                  )
+              ),
+              Center(
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    color: Colors.white,
+                    child:  ListTile(
+                      title: Text('Triceps', style: TextStyle(fontSize: 15, color: Colors.blueGrey), textAlign: TextAlign.center,),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context)  => GroupEx(
+                                    groupTitle: "Triceps"
+                                )
+                            )
+                        );
+                      },
+                    ),
+                  )
+              ),
+              Center(
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    color: Colors.white,
+                    child:  ListTile(
+                      title: Text('Shoulders', style: TextStyle(fontSize: 15, color: Colors.blueGrey), textAlign: TextAlign.center,),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context)  => GroupEx(
+                                    groupTitle: "Shoulders"
+                                )
+                            )
+                        );
+                      },
+                    ),
+                  )
+              ),
+              Center(
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    color: Colors.white,
+                    child:  ListTile(
+                      title: Text('Legs', style: TextStyle(fontSize: 15, color: Colors.blueGrey), textAlign: TextAlign.center,),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context)  => GroupEx(
+                                    groupTitle: "Legs"
+                                )
+                            )
+                        );
+                      },
+                    ),
+                  )
+              ),
+            ]
+        )
+    );
+  }
+}
